3f6ec9ad1720c5f636e8f3608cb943dfac974649
Stephen <clarksj4@student.op.ac.nz>
2016-07-26 18:20:39 +1200

    Add super triangle calculation

--- Assets/Framework/DelaunayTriangulation.cs
+++ Assets/Framework/DelaunayTriangulation.cs
@@ -11,100 +11,82 @@ namespace InventorySimulator
 
     public class DelaunayTriangulation
     {
+        public static Vector3 superTriangleExtents = new Vector3(100, 100, 100);
+
         private TriGraph graph;
 
         public DelaunayTriangulation()
         {
             graph = new TriGraph();
+
+            // Create super triangle -> 4 trianlges that make a triangle-based pyramid
+            // Super triangle is large enough that it encorporates all points in input set
+            GraphNode top = graph.AddNode(new Vector3(0, superTriangleExtents.y, 0));
+            GraphNode bottomLeft = graph.AddNode(new Vector3(-superTriangleExtents.x, -superTriangleExtents.y, -superTriangleExtents.z));
+            GraphNode bottomRight = graph.AddNode(new Vector3(superTriangleExtents.x, -superTriangleExtents.y, -superTriangleExtents.z));
+            GraphNode bottomBack = graph.AddNode(new Vector3(0, -superTriangleExtents.y, superTriangleExtents.z));
+
+            graph.AddEdge(top, bottomLeft);
+            graph.AddEdge(top, bottomRight);
+            graph.AddEdge(top, bottomBack);
+            graph.AddEdge(bottomLeft, bottomRight);
+            graph.AddEdge(bottomLeft, bottomBack);
+            graph.AddEdge(bottomRight, bottomBack);
         }
 
-        // TODO: USE BOWER-WATSON ALGORITHM INSTEAD (wikipedia ya'll)
         public void Insert(Vector3 vector)
         {
-            // Special case: first node to be inserted - no checks required - no edges to be made
-            if (graph.Nodes.Count == 0)
+            // List for storing triangles that violate Delaunay property
+            List<GraphTriangle> guiltyTriangles = new List<GraphTriangle>();
+
+            // Find triangles that the point violates the Delaunay-ness of
+            for (int j = 0; j < graph.Triangles.Count; j++)
             {
-                graph.AddNode(vector);
+                // Check if point is inside the circumsphere of the current triangle
+                if (graph.Triangles[j].InsideCircumsphere(vector))
+                    guiltyTriangles.Add(graph.Triangles[j]);    // If so, Delaunay-ness has been violated and the triangle is marked as guilty
             }
 
-            // Special case: second or third node to be inserted - no checked required
-            else if (graph.Nodes.Count == 1 || graph.Nodes.Count == 2)
+            // If there are guilty triangles
+            if (guiltyTriangles.Count > 0)
             {
-                // Add node
-                GraphNode newNode = graph.AddNode(vector);
-
-                // Add edges between all nodes
-                foreach (GraphNode node in graph.Nodes)
-                    graph.AddEdge(newNode, node);
-            }
-
-            else
-            {
-                // List for storing triangles that violate Delaunay property
-                List<GraphTriangle> guiltyTriangles = new List<GraphTriangle>();
-
-                // Find triangles that the point violates the Delaunay-ness of
-                for (int j = 0; j < graph.Triangles.Count; j++)
+                // Generate list of inside edges of the polygonal hole left by removing the guilty triangles
+                List<GraphEdge> insideEdges = new List<GraphEdge>();
+                for (int i = 0; i < guiltyTriangles.Count - 1; i++)
                 {
-                    // Check if point is inside the circumsphere of the current triangle
-                    if (graph.Triangles[j].InsideCircumsphere(vector))
-                        guiltyTriangles.Add(graph.Triangles[j]);    // If so, Delaunay-ness has been violated and the triangle is marked as guilty
-                }
-
-                // If there are guilty triangles
-                if (guiltyTriangles.Count > 0)
-                {
-                    // Generate list of inside edges of the polygonal hole left by removing the guilty triangles
-                    List<GraphEdge> insideEdges = new List<GraphEdge>();
-                    for (int i = 0; i < guiltyTriangles.Count - 1; i++)
+                    // Check each edge of each guilty triangle
+                    foreach (GraphEdge edge in guiltyTriangles[i].GetEdges())
                     {
-                        // Check each edge of each guilty triangle
-                        foreach (GraphEdge edge in guiltyTriangles[i].GetEdges())
+                        // Iterate through remaining guiltyTriangles checking if any contain the current edge
+                        for (int j = i + 1; j < guiltyTriangles.Count; j++)
                         {
-                            // Iterate through remaining guiltyTriangles checking if any contain the current edge
-                            for (int j = i + 1; j < guiltyTriangles.Count; j++)
+                            // If another guilty triangle contains the same edge, the edge is an inner edge
+                            if (guiltyTriangles[j].Contains(edge))
                             {
-                                // If another guilty triangle contains the same edge, the edge is an inner edge
-                                if (guiltyTriangles[j].Contains(edge))
-                                {
-                                    insideEdges.Add(edge);
-                                    break;                                      // Break - no need to keep checking other triangles
-                                }
+                                insideEdges.Add(edge);
+                                break;                                      // Break - no need to keep checking other triangles
                             }
                         }
                     }
-
-                    // Remove all inside edges from graph
-                    foreach (GraphEdge insideEdge in insideEdges)
-                        graph.RemoveEdge(insideEdge);
-
-                    // Add vector to graph
-                    GraphNode node = graph.AddNode(vector);
-
-                    // Triangulate the hole
-                    foreach (GraphTriangle guiltyTriangle in guiltyTriangles)
-                    {
-                        foreach (GraphNode guiltyNode in guiltyTriangle.GetNodes())
-                            graph.AddEdge(node, guiltyNode);
-                    }
-
-                    //List<GraphNode> affectedNodes = RemoveTriangles(graph, guiltyTriangles);    // Remove triangles
-                    //TriangulateHole(graph, vector, affectedNodes);                              // Triangulate the hole
                 }
 
-                else // No guilty triangles
-                {
-                    // Get two closest nodes to the current vector
-                    List<GraphNode> closest = graph.Closest(vector, 2);
+                // Remove all inside edges from graph
+                foreach (GraphEdge insideEdge in insideEdges)
+                    graph.RemoveEdge(insideEdge);
 
-                    // Add vector as node and connect an edge from the two closest nodes to this node
-                    GraphNode node = graph.AddNode(vector);
-                    foreach (GraphNode closeNode in closest)
-                        graph.AddEdge(node, closeNode);
+                // Add vector to graph
+                GraphNode node = graph.AddNode(vector);
+
+                // Triangulate the hole
+                foreach (GraphTriangle guiltyTriangle in guiltyTriangles)
+                {
+                    foreach (GraphNode guiltyNode in guiltyTriangle.GetNodes())
+                        graph.AddEdge(node, guiltyNode);
                 }
             }
         }
 
+        // TODO: Find triangle large enough to encorporate ALL insertion vectors
         public void Insert(Vector3[] insertionVectors)
         {
             for (int i = 0; i < insertionVectors.Length; i++)
