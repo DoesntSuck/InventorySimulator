70cc9f765de243925ed7e16320cfb4c9f11920dd
Stephen <clarksj4@student.op.ac.nz>
2016-07-26 17:43:52 +1200

    Bowyer-Watson algorithm implementation

--- Assets/Framework/DelaunayTriangulation.cs
+++ Assets/Framework/DelaunayTriangulation.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UnityEngine;
 using UnityExtension;
 
@@ -19,7 +17,6 @@ namespace InventorySimulator
         {
             graph = new TriGraph();
         }
-
 
         // TODO: USE BOWER-WATSON ALGORITHM INSTEAD (wikipedia ya'll)
         public void Insert(Vector3 vector)
@@ -57,8 +54,42 @@ namespace InventorySimulator
                 // If there are guilty triangles
                 if (guiltyTriangles.Count > 0)
                 {
-                    List<GraphNode> affectedNodes = RemoveTriangles(graph, guiltyTriangles);    // Remove triangles
-                    TriangulateHole(graph, vector, affectedNodes);                              // Triangulate the hole
+                    // Generate list of inside edges of the polygonal hole left by removing the guilty triangles
+                    List<GraphEdge> insideEdges = new List<GraphEdge>();
+                    for (int i = 0; i < guiltyTriangles.Count - 1; i++)
+                    {
+                        // Check each edge of each guilty triangle
+                        foreach (GraphEdge edge in guiltyTriangles[i].GetEdges())
+                        {
+                            // Iterate through remaining guiltyTriangles checking if any contain the current edge
+                            for (int j = i + 1; j < guiltyTriangles.Count; j++)
+                            {
+                                // If another guilty triangle contains the same edge, the edge is an inner edge
+                                if (guiltyTriangles[j].Contains(edge))
+                                {
+                                    insideEdges.Add(edge);
+                                    break;                                      // Break - no need to keep checking other triangles
+                                }
+                            }
+                        }
+                    }
+
+                    // Remove all inside edges from graph
+                    foreach (GraphEdge insideEdge in insideEdges)
+                        graph.RemoveEdge(insideEdge);
+
+                    // Add vector to graph
+                    GraphNode node = graph.AddNode(vector);
+
+                    // Triangulate the hole
+                    foreach (GraphTriangle guiltyTriangle in guiltyTriangles)
+                    {
+                        foreach (GraphNode guiltyNode in guiltyTriangle.GetNodes())
+                            graph.AddEdge(node, guiltyNode);
+                    }
+
+                    //List<GraphNode> affectedNodes = RemoveTriangles(graph, guiltyTriangles);    // Remove triangles
+                    //TriangulateHole(graph, vector, affectedNodes);                              // Triangulate the hole
                 }
 
                 else // No guilty triangles
