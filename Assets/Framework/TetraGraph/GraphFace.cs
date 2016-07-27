db87421ea3711a7f78a08fa8b987b20f93aa3898
Stephen <clarksj4@student.op.ac.nz>
2016-07-28 09:56:18 +1200

    Add TetraGraph

---
+++ Assets/Framework/TetraGraph/GraphFace.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Framework;
+
+namespace Framework.TetraGraph
+{
+    public class GraphFace
+    {
+        /// <summary>
+        /// The three nodes that this face is connected to
+        /// </summary>
+        public GraphNode[] Nodes { get; private set; }
+
+        public GraphFace(GraphNode node1, GraphNode node2, GraphNode node3)
+        {
+            Nodes = new GraphNode[] { node1, node2, node3 };
+        }
+
+        /// <summary>
+        /// Checks if this face is connected to the given node
+        /// </summary>
+        public bool Contains(GraphNode node)
+        {
+            return Nodes.Contains(node);
+        }
+
+        /// <summary>
+        /// Checks if this face is connected to the two given nodes
+        /// </summary>
+        public bool Contains(GraphNode node1, GraphNode node2)
+        {
+            return Nodes.Contains(node1) && Nodes.Contains(node2);
+        }
+
+        /// <summary>
+        /// Gets the third node in this faces set of nodes that is unique from the two given nodes
+        /// </summary>
+        public GraphNode GetOther(GraphNode node1, GraphNode node2)
+        {
+            foreach (GraphNode node in Nodes)
+            {
+                // If not node 1 or 2 it must be the third unique node
+                if (node != node1 && 
+                    node != node2)
+                    return node;
+            }
+
+            // If node wasn't found, something has gone wrong
+            throw new ArgumentException("Third unique node could not be found.");
+        }
+
+        /// <summary>
+        /// Finds the two other nodes that this face is connected to
+        /// </summary>
+        public GraphNode[] GetOthers(GraphNode node)
+        {
+            GraphNode[] others = new GraphNode[2];
+            int i = 0;
+            foreach (GraphNode other in Nodes)
+            {
+                if (other != node)
+                    others[i++] = other;                    // Method will throw index out of range exception if node is not contained in Nodes
+            }
+
+            return others;
+        }
+
+        /// <summary>
+        /// Checks if this face and the given face contain the same nodes
+        /// </summary>
+        public bool Equals(GraphFace other)
+        {
+            foreach (GraphNode node in Nodes)
+            {
+                // Atleast one node is different
+                if (!other.Contains(node))
+                    return false;
+            }
+
+            // All nodes are the same
+            return true;
+        }
+    }
+}
---
+++ Assets/Framework/TetraGraph/GraphNode.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Framework.TetraGraph
+{
+    public class GraphNode
+    {
+        /// <summary>
+        /// The collection of faces connected to this node
+        /// </summary>
+        public List<GraphFace> Faces { get; private set; }
+
+        /// <summary>
+        /// The vector data that this node stores
+        /// </summary>
+        public Vector3 Vector { get; private set; }
+
+        public GraphNode(Vector3 vector)
+        {
+            Vector = vector;
+            Faces = new List<GraphFace>();
+        }
+
+        /// <summary>
+        /// Adds the given face to this nodes collection of faces
+        /// </summary>
+        public void AddFace(GraphFace face)
+        {
+            Faces.Add(face);
+        }
+
+        /// <summary>
+        /// Finds and removes the given face from this nodes collection of faces
+        /// </summary>
+        public void RemoveFace(GraphFace face)
+        {
+            Faces.Remove(face);
+        }
+
+        /// <summary>
+        /// Finds and removes the face the connects this node to the given two nodes
+        /// </summary>
+        public void RemoveFace(GraphNode connectedNode1, GraphNode connectedNode2)
+        {
+            // Iterate through all faces connected to this node
+            for (int i = 0; i < Faces.Count; i++)
+            {
+                // If face contains THIS node and the two given nodes...
+                if (Faces[i].Contains(connectedNode1) &&
+                    Faces[i].Contains(connectedNode2))
+                {
+                    Faces.RemoveAt(i);                      // Remove the face
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Finds and removes all faces that are connected to both this node and the given node
+        /// </summary>
+        public void RemoveFaces(GraphNode connectedNode)
+        {
+            // Iterate through all faces connected to this node
+            for (int i = 0; i < Faces.Count; i++)
+            {
+                // If face contains THIS node and the given node...
+                if (Faces[i].Contains(connectedNode))
+                    Faces.RemoveAt(i--);                    // Remove the face
+            }
+        }
+
+        /// <summary>
+        /// Finds the face that connects this node with both of the given nodes
+        /// </summary>
+        public GraphFace GetFace(GraphNode connectedNode1, GraphNode connectedNode2)
+        {
+            foreach (GraphFace face in Faces)
+            {
+                // If face contains THIS node and the two given nodes
+                if (face.Contains(connectedNode1) &&
+                    face.Contains(connectedNode2))
+                    return face;
+            }
+
+            // No face in collection contains all three nodes
+            return null;
+        }
+
+        /// <summary>
+        /// Iterates through the collection of faces that share this node and the given node
+        /// </summary>
+        public IEnumerable<GraphFace> GetFaces(GraphNode connectedNode)
+        {
+            foreach (GraphFace face in Faces)
+            {
+                // If face contains THIS node and the given node
+                if (face.Contains(connectedNode))
+                    yield return face;
+            }
+        }
+
+        /// <summary>
+        /// Checks if this node contains a face connecting it to the given node
+        /// </summary>
+        public bool ConnectsTo(GraphNode other)
+        {
+            foreach (GraphFace face in Faces)
+            {
+                // If face contains THIS node and the given node
+                if (face.Contains(other))
+                    return true;
+            }
+
+            // No face connects this node to the given node
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if there is a face connecting this node to the two given nods
+        /// </summary>
+        public bool ConnectsTo(GraphNode node1, GraphNode node2)
+        {
+            foreach (GraphFace face in Faces)
+            {
+                // If face connects THIS node to the two given nodes
+                if (face.Contains(node1) &&
+                    face.Contains(node2))
+                    return true;
+            }
+
+            // No face connects this node to the two given nodes
+            return false;
+        }
+    }
+}
